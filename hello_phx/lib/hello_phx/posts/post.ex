1e85927915b34e6c0f36c0ead405c9f5889f10e2
raiden.me(김의현) <raiden.me@kakaocorp.com>
2024-12-23 16:51:02 +0900

    update: @derive spec 추가

--- hello_phx/lib/hello_phx/posts/post.ex
+++ hello_phx/lib/hello_phx/posts/post.ex
@@ -1,13 +1,44 @@
 defmodule HelloPhx.Posts.Post do
   use Ecto.Schema # 고정 값 mixin 같은거임
+  alias HelloPhx.Repo
 
+  import Ecto.Changeset
+
+  require Logger
 
   ## 맵핑되는 테이블 네임
+
+  ## json 직렬화 하려면 이거 던져야함. 이거 설정 안해서 3시간 뻘짓함ㅋㅋ
+  @derive {Jason.Encoder, only: [:id, :title, :content, :author, :inserted_at, :updated_at]}
   schema "posts" do
     field :title, :string
-    field :context, :string
+    field :content, :string
     field :author, :string
     timestamps()
+  end
+
+  def change(posts, att) do
+    posts
+    |> cast(att, [:title, :content, :author])
+    |> validate_required([:title, :content, :author])
+    |> Repo.insert();
+
+    # {:ok, posts}
+  end
+
+
+  @spec changeset(Ecto.Schema.t(), map) :: Ecto.Changeset.t()
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :content, :author])
+    |> validate_required([:title, :content, :author])
+  end
+
+  @spec create_post(map) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
+  def create_post(attrs) do
+    %__MODULE__{}
+    |> changeset(attrs)
+    |> Repo.insert()
   end
 
 end
--- hello_phx/lib/hello_phx_web/controllers/posts_controller.ex
+++ hello_phx/lib/hello_phx_web/controllers/posts_controller.ex
@@ -1,5 +1,7 @@
 defmodule HelloPhxWeb.PostsController do
   use HelloPhxWeb, :controller
+  alias HelloPhx.Posts.Post
+  alias HelloPhx.Repo
   require Logger
   # use Todos.{}
 
@@ -10,8 +12,25 @@ defmodule HelloPhxWeb.PostsController do
   def home(conn, _param) do conn |> json(%{data: "123"}) end
   def edit(conn, _param) do conn |> json(%{data: "123"} )end
   def create(conn, param) do
-    Logger.info("New Post #{inspect(param)}")
-    conn |> json(param)
+    # data = Posts.Post.change(%Posts.Post{}, param)
+    # Logger.info("New Post #{inspect(data)}")
+
+    # # data2 = Ecto.Changeset.apply_changes(data)
+
+    # json(conn, param)
+
+    case Post.create_post(param) do
+      {:ok, post} ->
+        Logger.info("New Post #{inspect(post)}")
+        json(conn, post)
+
+      {:error, changeset} ->
+        Logger.error("Invalid Post #{inspect(changeset.errors)}")
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{errors: changeset.errors})
+    end
+
   end
   def show(conn, _param) do conn |> json(%{data: "123"} )end
 end
