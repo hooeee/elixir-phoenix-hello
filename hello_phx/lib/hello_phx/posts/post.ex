c85be1f1ee5122ff30cddb8a1944daa3146bd86c
raiden.me(김의현) <raiden.me@kakaocorp.com>
2024-12-24 17:38:50 +0900

    feature: todo ecto crud

--- hello_phx/lib/hello_phx/posts/post.ex
+++ hello_phx/lib/hello_phx/posts/post.ex
@@ -1,13 +1,60 @@
 defmodule HelloPhx.Posts.Post do
+  alias HelloPhx.Repo
   use Ecto.Schema # 고정 값 mixin 같은거임
-
+  import Ecto.Changeset
+  import Ecto.Query
 
   ## 맵핑되는 테이블 네임
+  # @derive {Jason.Encoder, only: [:id, :title, :content, :author, :inserted_at, :updated_at]}
   schema "posts" do
     field :title, :string
-    field :context, :string
+    field :content, :string
     field :author, :string
     timestamps()
+  end
+
+
+  def select_where(date) do
+    from(HelloPhx.Posts.Post)
+    |> where([p], p.inserted_at > ^date)
+    |> Repo.all()
+
+  end
+
+  def select_all() do
+    from(HelloPhx.Posts.Post)
+    |> select([p], %{id: p.id, title: p.title, content: p.content, author: p.author})
+    |> where([p], like(p.author, "%rai%"))
+    |> or_where([p], p.author == "abc")
+    |> order_by([p], desc: p.id)
+    |> Repo.all()
+    |> IO.inspect(label: "select_all")
+  end
+
+
+  @spec update(integer(), String.t()) :: {:ok, %HelloPhx.Posts.Post{}} | {:error, Ecto.Changeset.t()}
+  def update(key , title) do
+    post = Repo.get(HelloPhx.Posts.Post, key)
+    post
+    |> changeset(%{title: title})
+    |> Repo.update()
+  end
+
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :content, :author])
+    |> validate_required([:title, :content, :author])
+  end
+
+  def create_post(attr)  do
+    %__MODULE__{}
+    |> changeset(attr)
+    |> HelloPhx.Repo.insert()
+    |> case do
+      {:ok, post} -> {:ok, %{id: post.id, title: post.title, content: post.content, author: post.author}}
+      {:error, changeset} -> {:error, changeset}
+    end
+    |> IO.inspect(label: "attr")
   end
 
 end
--- hello_phx/lib/hello_phx_web/controllers/posts_controller.ex
+++ hello_phx/lib/hello_phx_web/controllers/posts_controller.ex
@@ -1,17 +1,32 @@
 defmodule HelloPhxWeb.PostsController do
   use HelloPhxWeb, :controller
   require Logger
+  alias HelloPhx.Posts.Post
   # use Todos.{}
 
   def index(conn, _params) do
-    conn |> json(%{data: "New Post"})
+    data = Post.select_all()
+    json(conn, data)
   end
 
   def home(conn, _param) do conn |> json(%{data: "123"}) end
   def edit(conn, _param) do conn |> json(%{data: "123"} )end
   def create(conn, param) do
-    Logger.info("New Post #{inspect(param)}")
-    conn |> json(param)
+    case Post.create_post(param) do
+      {:ok, post} ->
+        conn
+        |> put_status(:created)
+        |> json(%{data: post})
+      {:error, changeset} ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{errors: changeset.errors})
+
+    end
+
+    # Post.create_post(param)
+    # Logger.info("New Post #{inspect(param)}")
+    # conn |> json(param)
   end
   def show(conn, _param) do conn |> json(%{data: "123"} )end
 end
